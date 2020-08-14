6b8442bf6ccc40f82719f3858bcb4137f13f58bb
JasemDuncan <jasemvalencia@gmail.com>
2020-08-14 17:30:08 -0500

    Finish SQL_select_in_select

--- SQL_select_in_select.sql
+++ SQL_select_in_select.sql
@@ -29,3 +29,20 @@ SELECT name
  WHERE gdp > ALL(SELECT gdp
                            FROM world
                           WHERE continent='Europe' and gdp>0)
+--7--
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y WHERE y.continent=x.continent and area>0)
+--8--
+SELECT continent, name FROM world x
+WHERE name =(SELECT y.name FROM world y WHERE y.continent=x.continent 
+ORDER BY name LIMIT 1)
+--9--
+SELECT name, continent, population FROM world x
+WHERE 25000000>=ALL
+(SELECT y.population FROM world y WHERE x.continent=y.continent and population>0)
+--10--
+SELECT name, continent FROM world x
+WHERE x.population >=ALL
+(SELECT y.population*3 FROM world y 
+WHERE x.continent=y.continent and population>0 AND x.name!=y.name)
